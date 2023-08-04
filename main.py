32c9ae5b4648f865772b6c6d59d62d10017d8152
amay <amaytrivedi1212@gmail.com>
2023-08-04 23:30:06 +0530

    adding model evaluation changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from cifar10Project.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from cifar10Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from cifar10Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from cifar10Project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from cifar10Project.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 if __name__ == '__main__':
     logger.info("This is the first custom log!!")
@@ -40,6 +41,16 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
         data_ingestion = ModelTrainerTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    STAGE_NAME = "Model evaluation stage"
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_ingestion = ModelEvaluationTrainingPipeline()
         data_ingestion.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
